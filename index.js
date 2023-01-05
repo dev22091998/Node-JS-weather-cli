5fe899d04b5d5676b78dcab06ea81dbc9c076d66
dev22091998 <forgithub756@gmail.com>
2023-01-05 21:56:14 +0500

    Axios bilan APIni oldim

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import getArgs  from './helpers/args.js';
+import { getWeather } from './services/api.services.js';
 import { printError, printSucces, printHelp } from './services/log.service.js';
-import { saveKeyValue } from './services/storage.service.js';
+import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
 const saveToken = async token => {
+    if(! token.length) {
+        printError('Token doesn\'t exist')
+        return
+    }
     try {
-        await saveKeyValue('token', token)
+        await saveKeyValue(TOKEN_DICTIONARY.token, token)
         printSucces('Token was saved succesfully')
     } catch (err) {
         printError(err.message)
@@ -12,7 +17,7 @@ const saveToken = async token => {
 
 const startCli = () => {
     const args = getArgs(process.argv)
-    // console.log(args) 
+    // console.log(args)  
     if(args.h){
        printHelp()
     }
@@ -22,6 +27,6 @@ const startCli = () => {
     if(args.t){
         return saveToken(args.t)
     }
-    
+    getWeather('uzbekistan')
 }
 startCli()
